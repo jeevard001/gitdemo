140b1defcf8a9994ff1bfd431c1e41a45679d684
Jeeva R <jeevard001@gmail.com>
2023-07-11 21:28:00 +0530

    demo git

---
+++ src/test/java/net/jeeva/assetArtworks/GitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jeeva.assetArtworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
